32a78ac64b194bac69c42e3bd92307471446b516
ProgNuha <mufida.nuha.tif17@polban.ac.id>
2019-10-21 09:41:49 +0700

    edit loader view

--- application/controllers/Reviewer.php
+++ application/controllers/Reviewer.php
@@ -2,27 +2,37 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Reviewer extends CI_Controller {
+	function __construct(){
+		parent::__construct();
+		// $this->load->model('proposal');
+		// $this->load->database(); // load database
+		$this->load->helper('url');
+	}
+
 	public function index()
 	{
-		$this->load->view('templates/reviewer/header');
-		$this->load->view('templates/reviewer/sidebar');
-		$this->load->view('templates/reviewer/dashboard');
-		$this->load->view('templates/reviewer/footer');
+		$data['content'] = $this->template();
+		$this->load->view('templates/reviewer/dashboard',$data);
+	}
+
+	public function template()
+	{
+		$data['content'] = $this->load->view('templates/reviewer/header');
+		$data['content'] = $this->load->view('templates/reviewer/sidebar');
+		$data['content'] = $this->load->view('templates/reviewer/footer');
+
+		return $this->load->view('templates/reviewer/template',$data);
 	}
 
 	public function list_proposal()
 	{
-		$this->load->view('templates/reviewer/header');
-		$this->load->view('templates/reviewer/sidebar');
-		$this->load->view('templates/reviewer/list_proposal');
-		$this->load->view('templates/reviewer/footer');
+		$data['content'] = $this->template();
+		$this->load->view('list_proposal',$data);
 	}
 
 	public function list_review_proposal()
 	{
-		$this->load->view('templates/reviewer/header');
-		$this->load->view('templates/reviewer/sidebar');
-		$this->load->view('templates/reviewer/list_review_proposal');
-		$this->load->view('templates/reviewer/footer');
+		$data['content'] = $this->template();
+		$this->load->view('list_review_proposal',$data);
 	}
 }
--- application/controllers/Student.php
+++ application/controllers/Student.php
@@ -2,27 +2,37 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Student extends CI_Controller {
+	function __construct(){
+		parent::__construct();
+		// $this->load->model('student');
+		// $this->load->database(); // load database
+		$this->load->helper('url');
+	}
+
+	public function template()
+	{
+		$data['content'] = $this->load->view('templates/student/header');
+		$data['content'] = $this->load->view('templates/student/sidebar');
+		$data['content'] = $this->load->view('templates/student/footer');
+
+		return $this->load->view('templates/student/template',$data);
+	}
+
 	public function index()
 	{
-		$this->load->view('templates/student/header');
-		$this->load->view('templates/student/sidebar');
-		$this->load->view('templates/student/dashboard');
-		$this->load->view('templates/student/footer');
+		$data['content'] = $this->template();
+		$this->load->view('templates/student/dashboard',$data);
     }
     
     public function proposal_submission()
 	{
-		$this->load->view('templates/student/header');
-		$this->load->view('templates/student/sidebar');
-		$this->load->view('templates/student/proposal_submission');
-		$this->load->view('templates/student/footer');
+		$data['content'] = $this->template();
+		$this->load->view('proposal_submission',$data);
     }
     
     public function your_submission()
 	{
-		$this->load->view('templates/student/header');
-		$this->load->view('templates/student/sidebar');
-		$this->load->view('templates/student/your_submission');
-		$this->load->view('templates/student/footer');
+		$data['content'] = $this->template();
+		$this->load->view('/your_submission',$data);
 	}
 }
