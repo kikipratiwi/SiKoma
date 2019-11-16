c4e9e4f2a58b0f7a5180b772015240a33994dd39
Kiki <kiki.pratiwi.tif17@polban.ac.id>
2019-11-16 17:35:25 +0700

    add Authentication class

---
+++ application/controllers/Home.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+	function __construct(){
+		parent::__construct();
+		// $this->load->model('proposal');
+		// $this->load->database(); // load database
+		$this->load->helper('url');
+	}
+
+	public function index()
+	{
+		$data['content'] = $this->template();
+		$this->load->view('terms',$data);
+	}
+    public function template()
+	{
+		$data['content'] = $this->load->view('templates/login/header');
+		$data['content'] = $this->load->view('templates/login/footer');
+
+		return $this->load->view('templates/template',$data);
+	}
+
+}
--- application/views/change_password.php
+++ application/views/change_password.php
@@ -5,24 +5,24 @@
 
 			<div class="col-sm-12">
 				<div class="login-card card-block">
-					<form class="md-float-material" type="POST" action="<?php echo base_url(); ?>index.php/Authentication/login">
+					<form class="md-float-material" type="POST" action="<?php echo base_url(); ?>index.php/Authentication/act_change_password">
 						<div class="text-center">
 							<img src="<?php echo base_url(); ?>assets/images/favicon.png" alt="logo">
 						</div>
 						<h3 class="text-center txt-primary">
-							Login ke akun anda
+							Ubah Kata Sandi
 						</h3>
 						<div class="md-input-wrapper">
-							<input type="text" class="md-form-control" />
-							<label>NIM/NIDN/</label>
+							<input type="password" name="current_password" class="md-form-control" />
+							<label>Kata sandi saat ini</label>
 						</div>
 						<div class="md-input-wrapper">
-							<input type="password" class="md-form-control" />
-							<label>Kata sandi</label>
+							<input type="password" name="new_password" class="md-form-control" />
+							<label>Kata sandi baru</label>
 						</div>
 						<div class="row">
 							<div class="col-xs-10 offset-xs-1">
-								<button type="submit" class="btn btn-primary btn-md btn-block waves-effect text-center m-b-20">LOGIN</button>
+								<button type="submit" class="btn btn-primary btn-md btn-block waves-effect text-center m-b-20">SIMPAN</button>
 							</div>
 						</div>
 						<!-- <div class="card-footer"> -->
