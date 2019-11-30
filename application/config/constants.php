ed05810e642821f67e43ab768d120600a7f12ff5
Kiki <kiki.pratiwi.tif17@polban.ac.id>
2019-11-30 14:50:01 +0700

    change url_api constant

--- application/config/constants.php
+++ application/config/constants.php
@@ -84,5 +84,5 @@ defined('EXIT_DATABASE')       OR define('EXIT_DATABASE', 8); // database error
 defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
 
-define('API_URL','http://192.168.43.24:8000'); //API URL
-// define('API_URL','http://localhost:8000'); //API localhost
+// define('API_URL','http://192.168.43.24:8000'); //API URL
+define('API_URL','http://localhost:8000'); //API localhost
